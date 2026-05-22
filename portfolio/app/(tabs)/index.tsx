b1dc69bee7747eaf0b5cad31ed52f7671b901bba
unknown <hei.nekena.34@gmail.com>
2026-05-22 16:11:55 +0200

    index

--- portfolio/app/(tabs)/index.tsx
+++ portfolio/app/(tabs)/index.tsx
@@ -1,22 +1,23 @@
 import React from 'react';
-import {View, Text, StyleSheet } from 'react-native';
-import { Image } from 'react-native';
-
+import { View, Text, StyleSheet, Image } from 'react-native'; 
 import Avatar from '@/assets/images/avatar.png';
 
 const index = () => {
   return (
-    <View className = "Container">
+    <View className="Container">
       <View className='Container-image'>
-        <Image source={Avatar} />
+        <Image source={Avatar} style={{ width: 150, height: 150 }} />
       </View>
       <View className='Container-text'>
-        <Text>
-          
+        <Text style={{ fontSize: 24, fontWeight: 'bold' }}>
+          Welcome to my portfolio 
         </Text>
+        <button>
+            See Portfolio
+        </button>
       </View>
     </View>
-  )
+  );
 }
 
-export default index
+export default index;
